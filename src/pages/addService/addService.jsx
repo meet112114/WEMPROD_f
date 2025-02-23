29a766928e7894bf3fe36202adfcf0db89d5bfa5
meet112114 <118681492+meet112114@users.noreply.github.com>
2025-02-23 21:16:06 +0530

    Update addService.jsx

--- src/pages/addService/addService.jsx
+++ src/pages/addService/addService.jsx
@@ -138,8 +138,8 @@ const ServiceForm = () => {
       const result = await response.json();
       if (response.ok) {
         alert("Service added successfully!");
-        setService( name: "",     serviceType:"",     description: "",
-    images: [],     venueList: [],     plans: [] );
+        setService({ name: "",     serviceType:"",     description: "",
+    images: [],     venueList: [],     plans: [] });
           setImageFiles([]);
           
       } else {
